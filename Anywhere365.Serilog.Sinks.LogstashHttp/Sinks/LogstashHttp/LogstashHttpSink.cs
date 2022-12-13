17ef4bde30af18756a78df82bd3d0841559cc1c3
Alain <alain.vandenberg@anywhere365.net>
2022-12-13 14:56:01 +0100

    Change project name to signify it is our own version of this library

---
+++ Anywhere365.Serilog.Sinks.LogstashHttp/Sinks/LogstashHttp/LogstashHttpSink.cs
@@ -0,0 +1,133 @@
+﻿// Copyright 2017 Aleksandr Sukhodko
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Nito.AsyncEx;
+
+using Serilog.Events;
+using Serilog.Sinks.PeriodicBatching;
+
+namespace Serilog.Sinks.LogstashHttp
+{
+    /// <summary>
+    ///     Writes log events as documents to ElasticSearch.
+    /// </summary>
+    public class LogstashHttpSink : IBatchedLogEventSink
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private static readonly AsyncLock Mutex = new AsyncLock();
+
+        private readonly LogstashHttpSinkState _state;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogstashHttpSink"/> class with the provided options
+        /// </summary>
+        /// <param name="options">
+        /// Options configuring how the sink behaves, may NOT be null
+        /// </param>
+        public LogstashHttpSink(LogstashHttpSinkOptions options)
+        {
+            _state = LogstashHttpSinkState.Create(options);
+        }
+
+        public Task OnEmptyBatchAsync() => Task.CompletedTask;
+
+        /// <summary>
+        /// Emit a batch of log events, running to completion synchronously.
+        /// </summary>
+        /// <param name="events">
+        /// The events to emit.
+        /// </param>
+        /// <remarks>
+        /// Override either
+        ///     <see cref="M:Serilog.Sinks.PeriodicBatching.PeriodicBatchingSink.EmitBatch(System.Collections.Generic.IEnumerable{Serilog.Events.LogEvent})"/>
+        ///     or
+        ///     <see cref="M:Serilog.Sinks.PeriodicBatching.PeriodicBatchingSink.EmitBatchAsync(System.Collections.Generic.IEnumerable{Serilog.Events.LogEvent})"/>
+        ///     , not both.
+        /// </remarks>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public async Task EmitBatchAsync(IEnumerable<LogEvent> events)
+        {
+            // ReSharper disable PossibleMultipleEnumeration
+            if (events == null || !events.Any()) return;
+
+            if (_state.Options.JsonLinesBatch)
+            {
+                try
+                {
+                    var sw = new StringWriter();
+                    foreach (var e in events)
+                    {
+                        _state.Formatter.Format(e, sw);
+
+                        sw.WriteLine();
+                    }
+                    var logData = sw.ToString();
+
+                    await PostLogDataAsync(logData);
+                }
+                catch (Exception ex)
+                {
+                    // Debug me
+                    throw ex;
+                }
+            }
+            else
+            {
+                foreach (var e in events)
+                {
+                    try
+                    {
+                        var sw = new StringWriter();
+                        _state.Formatter.Format(e, sw);
+                        var logData = sw.ToString();
+
+                        await PostLogDataAsync(logData);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Debug me
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Posts the serialized events log data to logstash uri
+        /// </summary>
+        /// <param name="logData"></param>
+        /// <returns></returns>
+        private async Task PostLogDataAsync(string logData)
+        {
+            var stringContent = new StringContent(logData);
+            stringContent.Headers.Remove("Content-Type");
+            stringContent.Headers.Add("Content-Type", _state.Options.ContentType);
+
+            // Using singleton of HttpClient so we need ensure of thread safety. Just use LockAsync.
+            using (await Mutex.LockAsync().ConfigureAwait(false))
+            {
+                await HttpClient.PostAsync(_state.Options.LogstashUri, stringContent).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ Anywhere365.Serilog.Sinks.LogstashHttp/Sinks/LogstashHttp/LogstashHttpSinkOptions.cs
@@ -0,0 +1,70 @@
+// Copyright 2014 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace Serilog.Sinks.LogstashHttp
+{
+    /// <summary>
+    ///     Provides ElasticsearchSink with configurable options
+    /// </summary>
+    public class LogstashHttpSinkOptions : PeriodicBatching.PeriodicBatchingSinkOptions
+    {
+
+        /// <summary>
+        ///     Supplies culture-specific formatting information, or null.
+        /// </summary>
+        public IFormatProvider FormatProvider { get; set; }
+
+        /// <summary>
+        ///     When true fields will be written at the root of the json document
+        /// </summary>
+        public bool InlineFields { get; set; }
+
+        /// <summary>
+        ///     The minimum log event level required in order to write an event to the sink.
+        /// </summary>
+        public LogEventLevel? MinimumLogEventLevel { get; set; }
+
+        /// <summary>
+        ///     Customizes the formatter used when converting log events into ElasticSearch documents. Please note that the
+        ///     formatter output must be valid JSON
+        /// </summary>
+        public ITextFormatter CustomFormatter { get; set; }
+
+        /// <summary>
+        ///     Customizes the formatter used when converting log events into the durable sink. Please note that the formatter
+        ///     output must be valid JSON
+        /// </summary>
+        public ITextFormatter CustomDurableFormatter { get; set; }
+
+        /// <summary>
+        ///     Logstash Uri
+        /// </summary>
+        public string LogstashUri { get; set; }
+
+        /// <summary>
+        ///     Enables sending the batch of events at once as json lines to logstash. Defaults to false.
+        /// </summary>
+        public bool JsonLinesBatch { get; set; }
+
+        /// <summary>
+        ///     Content-Type request header used when posting logs to logstash. Defaults to "application/json"
+        /// </summary>
+        public string ContentType { get; set; } = "application/json";
+
+    }
+}
